fab534e7ba8c23703dd74682b76da09baea80bf4
Juan Dorado <fdorado985@gmail.com>
2020-09-13 12:55:26 -0500

    Add setup layouts and viewDidLoad lifecycle to ViewController module

--- I-VIPER/Module.xctemplate/UIViewControllergenerateEntityFile/___FILEBASENAME___/View/___FILEBASENAME___ViewController.swift
+++ I-VIPER/Module.xctemplate/UIViewControllergenerateEntityFile/___FILEBASENAME___/View/___FILEBASENAME___ViewController.swift
@@ -10,8 +10,25 @@ class ___VARIABLE_productName:identifier___ViewController: UIViewController {
   // MARK: - Properties
 
   var presenter: ___VARIABLE_productName:identifier___PresenterInterface?
+
+  // MARK: - View Lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureController()
+  }
 }
 
 // MARK: - ___VARIABLE_productName:identifier___ViewInterface
 
 extension ___VARIABLE_productName:identifier___ViewController: ___VARIABLE_productName:identifier___ViewInterface {}
+
+// MARK: - Setup Layouts
+
+extension ___VARIABLE_productName:identifier___ViewController {
+  private func configureController() {
+    setupLayouts()
+  }
+
+  private func setupLayouts() {}
+}
